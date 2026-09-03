3d1c9809c5dec35be97af080d40b26e10aa3708b
Gaythree Basarahalli <gayathri.basarahalli@gmail.com>
2026-09-03 09:57:04 +0200

    changes

--- gemini_api.py
+++ gemini_api.py
@@ -1,21 +1,22 @@
 import os
-import google.generativeai as genai
+from google import genai
 from dotenv import load_dotenv
 
-# Using Google Generative AI API
 # Loading environment variables from .env file
-
 load_dotenv()
+
 GENAI_API_KEY = os.getenv("GENAI_API_KEY")
-genai.configure(api_key=GENAI_API_KEY)
 
-#Gemini model initialization
-#using the latest Gemini model gemini-1.5-pro
+# Gemini client
+client = genai.Client(api_key=GENAI_API_KEY)
 
-model = genai.GenerativeModel("gemini-1.5-pro")
+# Gemini model
+model = "gemini-2.5-flash"
+
 
 # Function to ask Gemini model a question with context
 def ask_gemini(context, question):
+
     prompt = f"""Use the following text as context and answer the question in English, even if the input is in Swedish:
 
 CONTEXT:
@@ -24,8 +25,12 @@ CONTEXT:
 QUESTION:
 {question}
 """
-# call the Gemini model to generate content based on the prompt
-    response = model.generate_content(prompt)
 
-#return the generated answer
+    # Generate response
+    response = client.models.generate_content(
+        model=model,
+        contents=prompt
+    )
+
+    # Return generated answer
     return response.text.strip()
